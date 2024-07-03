dead9a48c0d0859c3ecf00ebb67471b6d09b150b
Krokoko <142137027+Kpokoko@users.noreply.github.com>
2024-07-04 00:12:28 +0500

    Add files via upload

---
+++ Entity/Block.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using nameless.AbstractClasses;
+using nameless.Interfaces;
+using nameless_game_branch.Tiles;
+
+namespace nameless_game_branch.Entity
+{
+    public class Block : Collider, IEntity
+    {
+        public Block(int x, int y)
+        {
+            Position = new Tile(x, y).Position;
+            SetCollision(this, 64, 64);
+        }
+
+        public Block() { }
+        public Vector2 Position { get; set; }
+        //public Vector2 TilePos { get => Tile.ConvertFromAbsoluteToTileCoordinats(Position); set { } }
+        int IGameObject.DrawOrder => 1;
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        { }
+
+        public void Update(GameTime gameTime)
+        { }
+    }
+}
---
+++ Serializer/Serialize.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using nameless.Interfaces;
+using nameless_game_branch.Entity;
+using nameless_game_branch.Tiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace nameless.Serialize
+{
+    public struct Serializer
+    {
+        public Serializer() { }
+        public void Serialize<T>(string sceneName, List<T> entities)
+            where T : IEntity
+        {
+            using (var writer = new StreamWriter(new FileStream(sceneName, FileMode.Create)))
+            {
+                var serializer = new XmlSerializer(entities.GetType());
+                Tile.GetPosInTileCoordinats(entities);
+                serializer.Serialize(writer, entities);
+            }
+        }
+        public List<T> Deserialize<T>(string sceneName)
+            where T : IEntity
+        {
+            using (var reader = new StreamReader(new FileStream(sceneName, FileMode.Open)))
+            {
+                var serializer = new XmlSerializer(typeof(List<Block>));
+                var scores = (List<T>)serializer.Deserialize(reader);
+                return scores;
+            }
+        }
+    }
+}
