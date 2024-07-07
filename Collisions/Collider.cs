aaf1cf07a95f5560ae836dfa9218195ca73c2f99
Stegosavr <144472273+Stegosavr@users.noreply.github.com>
2024-07-07 17:57:02 +0500

    Add files via upload

---
+++ Collisions/Collider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
+using nameless.Interfaces;
+
+namespace nameless.Collisions;
+
+public partial class Collider : ICollisionActor
+{
+    [XmlIgnore]
+    public IShapeF Bounds { get; protected set; }
+    protected ICollider entity;
+
+    public static void SetCollider(ICollider entity, int width, int height)
+    {
+        entity.collider = new Collider();
+        entity.collider.SetCollision(entity,width, height);
+    }
+
+    public virtual void SetCollision(IEntity entity, int width, int height)
+    {
+        Bounds = new RectangleF(entity.Position + Globals.Offset(width, height), new Size2(width, height));
+        this.entity = (ICollider)entity;
+        Globals.CollisionComponent.Insert(this);
+        Globals.Colliders.Add(this);
+    }
+
+    public virtual void OnCollision(CollisionEventArgs collisionInfo)
+    {
+        //entity.OnCollision(collisionInfo);
+    }
+
+    public void DrawCollision(SpriteBatch spriteBatch)
+    {
+        var rectBounds = (RectangleF)Bounds;
+        //spriteBatch.DrawRectangle(new RectangleF(new Point2(rectBounds.X - rectBounds.Width / 2, rectBounds.Y - rectBounds.Height / 2), rectBounds.Size), Color.Red, 5);
+        spriteBatch.DrawRectangle((RectangleF)Bounds, Color.Red,3);
+    }
+}
---
+++ Collisions/SideCollider/MainSideCollider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
+using nameless.Interfaces;
+
+namespace nameless.Collisions;
+
+public class MainSideCollider : DynamicCollider 
+{
+    private List<Side> collisionBuffer = new List<Side>();
+    private List<SideCollider> children = new List<SideCollider>();
+
+    public MainSideCollider() : base()
+    {
+        //Globals.CharacterColliders.Add(this);
+
+        foreach (var side in new[] { Side.Left, Side.Right, Side.Top, Side.Bottom })
+        {
+            var collider = new SideCollider(this,side);
+            children.Add(collider);
+        }
+    }
+
+    public void AddToBuffer(Side collisionSide)
+    {
+        if (!collisionBuffer.Contains(collisionSide))
+            collisionBuffer.Add(collisionSide);
+    }
+
+    public override void SetCollision(IEntity gameObject, int width, int height)
+    {
+        foreach (var child in children)
+        {
+            child.SetCollision(gameObject, width, height);
+        }
+
+        base.SetCollision(gameObject, width, height);
+    }
+
+    public void UpdateCollision()
+    {
+        if (collisionBuffer.Count > 0)
+        {
+            OnOuterCollision(collisionBuffer.ToList());
+        }
+        collisionBuffer.Clear();
+    }
+
+    public virtual void OnOuterCollision(List<Side> collisionInfo)
+    {
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        foreach (var child in children)
+        {
+            child.Bounds.Position = Bounds.Position + child.offset;
+        }
+    }
+}
