66f1b7d125f1f7c72ef8ab88cea03b7a42e664cc
Krokoko <142137027+Kpokoko@users.noreply.github.com>
2024-08-13 12:38:34 +0500

    audio fixes

--- Code/Engine/AudioManager.cs
+++ Code/Engine/AudioManager.cs
@@ -11,23 +11,36 @@ namespace nameless.Engine;
 public class AudioManager
 {
     public ContentManager Content;
-    private Dictionary<string, SoundEffect> _soundEffects;
+    private Dictionary<string, SoundEffectInstance> _soundEffects;
+    private Dictionary<string, int> _playingSounds;
 
     public void Initialize()
     {
-        var deathSound = Content.Load<SoundEffect>("Sounds/DeathSound");
-        var clickSound = Content.Load<SoundEffect>("Sounds/Click");
-        _soundEffects = new Dictionary<string, SoundEffect>
+        var deathSound = Content.Load<SoundEffect>("Sounds/DeathSound").CreateInstance();
+        var clickSound = Content.Load<SoundEffect>("Sounds/Click").CreateInstance();
+        _soundEffects = new Dictionary<string, SoundEffectInstance>
         {
             { "DeathSound", deathSound },
             { "Click", clickSound }
         };
+        _playingSounds = new Dictionary<string, int>()
+        {
+            { "DeathSound", 0 },
+            { "Click", 0 }
+        };
     }
 
-    public void PlaySound(string name, float volume = 1)
+    public void PlaySound(string name, float volume = 1, float pitch = 0)
     {
-        var instance = _soundEffects[name].CreateInstance();
+        var instance = _soundEffects[name];
+        if (_playingSounds[name] > 3)
+        {
+            instance.Stop();
+            _playingSounds[name]--;
+        }
         instance.Volume = volume;
+        instance.Pitch = pitch;
         instance.Play();
+        _playingSounds[name]++;
     }
 }
